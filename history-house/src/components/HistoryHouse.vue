6f981deae8e7999ea39f678ae6673efd8a0063f2
Vuejs <vuejs@zMacPro-7.local>
2020-01-18 14:15:56 +0100

    Update exercise with props

--- history-house/src/components/HistoryHouse.vue
+++ history-house/src/components/HistoryHouse.vue
@@ -12,31 +12,17 @@
         <option value="5a05dc58d45bd0a11bd5e070">Hufflepuff</option>
       </select>
     </div>
-    <div>
-        <h3>House card</h3>
-        <p>Founder: {{house.founder}}</p>
-        <p>Head of house: {{house.headOfHouse}}</p>
-        <p>House ghost: {{house.houseGhost}}</p>
-        <p>Mascot: {{house.mascot}}</p>
-        <p>School: {{house.school}}</p>
-        <br/>Members: <div v-for="(member, index) of house.members" :key="`member-${index}`">
-            {{member.name}}
-            </div>
-        <br/>Colors: <div v-for="(color, index) of house.colors" :key="`color-${index}`">
-            {{color}}
-        </div>
-
-        <br/>Values: <div v-for="(value, index) of house.values" :key="`value-${index}`">
-            {{value}}
-        </div>
-
-    </div>
+    <HouseCard :house="this.house"/>
   </div>
 </template>
 
 <script>
+import HouseCard from "./HouseCard";
 export default {
     name: "HistoryHouse",
+    components: {
+      HouseCard
+    },
     data(){
         return{
             apiKey: '$2a$10$/mQ60IF5cZe7iUGMReRt.eCLASP7wovwIdqK1XO5uLOezMUbx.ZTi',
---
+++ history-house/src/components/HouseCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h3>House card</h3>
+    <p>Founder: {{house.founder}}</p>
+    <p>Head of house: {{house.headOfHouse}}</p>
+    <p>House ghost: {{house.houseGhost}}</p>
+    <p>Mascot: {{house.mascot}}</p>
+    <p>School: {{house.school}}</p>
+    <br />Members:
+    <div v-for="(member, index) of house.members" :key="`member-${index}`">{{member.name}}</div>
+    <br />Colors:
+    <div v-for="(color, index) of house.colors" :key="`color-${index}`">{{color}}</div>
+
+    <br />Values:
+    <div v-for="(value, index) of house.values" :key="`value-${index}`">{{value}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "HouseCard",
+    props: {
+        house: {
+            required: true,
+            type: Array
+        }
+    }
+};
+</script>
+
+<style>
+</style>
