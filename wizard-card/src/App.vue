85ff6c91fe52baf27da42a2114b8644893294d5d
Vuejs <vuejs@zMacPro-7.local>
2020-01-18 11:18:48 +0100

    Add form and v-model

--- wizard-card/src/App.vue
+++ wizard-card/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WizardCard></WizardCard>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WizardCard from "./components/WizardCard";
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    WizardCard
   }
 }
 </script>
---
+++ wizard-card/src/components/WizardCard.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="flex-container">
+    <div>
+        <img src="../assets/hogwards-icon.jpg" style="max-width: 10%;">
+    </div>
+    <div>
+        <form>
+      <p>
+        Name:
+        <input type="text" name="name" v-model="name"/>
+      </p>
+      <p>
+        Alias:
+        <input type="text" name="alias" v-model="alias"/>
+      </p>
+      <p>
+        Role:
+        <input type="text" name="role" v-model="role"/>
+      </p>
+      <p>
+        Hause:
+        <input type="text" name="house" v-model="house"/>
+      </p>
+      <p>
+        School:
+        <input type="text" name="school" v-model="school"/>
+      </p>
+      <p>
+        Wand:
+        <input type="text" name="wand" v-model="wand"/>
+      </p>
+    </form>
+    </div>
+    <div>
+        <h2>Wizard Card</h2>
+        <p>
+            Name: {{name}}
+        </p>
+        <p>
+            Alias: {{alias}}
+        </p>
+        <p>
+            Role: {{role}}
+        </p>
+        <p>
+            House: {{house}}
+        </p>
+        <p>
+            School: {{school}}
+        </p>
+        <p>
+            Wand: {{wand}}
+        </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            name: "",
+            alias: "",
+            role: "",
+            house: "",
+            school: "",
+            wand: ""
+        }
+    },
+
+};
+</script>
+
+<style>
+.flex-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+</style>
