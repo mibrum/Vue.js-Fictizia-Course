2ed15ff3d421ff8e29edb3fb6baaba40a60f7705
Mireia <mireiabrumartinez@gmail.com>
2020-01-19 21:18:26 +0100

    Add sorting hat exercise

---
+++ sorting-hat/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+  <img src="./assets/sorting-hat.png" style="max-width: 20%;" alt="">
+  <sorting-hat/>
+  </div>
+</template>
+
+<script>
+import SortingHat from "./components/SortingHat";
+export default {
+  name: 'app',
+  components: {
+    SortingHat
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ sorting-hat/src/components/SortingHat.vue
@@ -0,0 +1,32 @@
+ <template>
+  <div>
+    <h1>Sorting Hat</h1>
+    <button @click="getHouse">Get your house</button>
+    <p>{{selectedHouse}}</p>
+    <img v-show="loader" src="../assets/loader.gif" style="max-width: 20%;" alt="">
+  </div>
+</template>
+ 
+ <script>
+export default {
+  data() {
+    return {
+      hogwartsHouses: ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"],
+      selectedHouse: "",
+      loader: false
+    };
+  },
+  methods: {
+      getHouse(){
+        this.loader = true;
+        setTimeout(() => {
+            this.loader = false;
+            this.selectedHouse = this.hogwartsHouses[Math.floor(Math.random()*this.hogwartsHouses.length)];
+        }, 5000)
+      }
+  }
+};
+</script>
+ 
+ <style>
+</style>
